ff110ea852c764798f46bb80922a69baa68ca54c
Daniel Trugman <dtrugman@gmail.com>
2026-04-03 16:20:37 +0100

    Remove .qory -> .config/qory migration code

--- lib/config/dir.go
+++ lib/config/dir.go
@@ -4,19 +4,13 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"runtime"
 
 	"github.com/dtrugman/qory/lib/profile"
 )
 
 const (
-	osWin         = "windows"
-	envWinAppData = "APPDATA"
-
-	dirDotQory    = ".qory"
-	dirUnixConfig = ".config"
-	dirQory       = "qory"
-	dirPerm       = 0700
+	dirDotQory = ".qory"
+	dirPerm    = 0700
 )
 
 func getConfigDir() (string, error) {
@@ -25,16 +19,8 @@ func getConfigDir() (string, error) {
 		return "", err
 	}
 
-	configDir := filepath.Join(userDir, dirDotQory)
-
-	if runtime.GOOS != osWin {
-		oldDir := filepath.Join(userDir, dirUnixConfig, dirQory)
-		if tryMigrateOldConfigDir(oldDir, configDir) {
-			return configDir, nil
-		}
-	}
-
-	return getOrCreateDir(configDir)
+	dir := filepath.Join(userDir, dirDotQory)
+	return getOrCreateDir(dir)
 }
 
 func getOrCreateDir(path string) (string, error) {
@@ -54,16 +40,4 @@ func getOrCreateDir(path string) (string, error) {
 	}
 
 	return path, nil
-}
-
-func tryMigrateOldConfigDir(oldDir, newDir string) bool {
-	if stat, err := os.Stat(oldDir); err != nil || !stat.IsDir() {
-		return false
-	}
-
-	if err := os.Rename(oldDir, newDir); err != nil {
-		return false
-	}
-
-	return true
 }
